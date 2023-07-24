9517e23d5d6369774831b4c9d0e1b5bae5c38fae
Hazem Magdy <hhazem0010@gmail.com>
2023-07-24 13:57:33 -0700

    seperate unsigned int function

--- functions.c
+++ functions.c
@@ -138,30 +138,3 @@ count += _putchar('0');
 
 return (count);
 }
-
-/**
- * print_unsigned_int - Print an unsigned integer.
- * @args: A list of parameters passed as variadic function parameters.
- *
- * Return: An integer.
- */
-int print_unsigned_int(va_list args)
-{
-unsigned int n, div;
-int count;
-
-div = 1;
-n = va_arg(args, unsigned int);
-while ((n / div) > 9)
-{
-div *= 10;
-}
-while (div > 0)
-{
-count += _putchar((n / div) + '0');
-n %= div;
-div /= 10;
-}
-
-return (count);
-}
